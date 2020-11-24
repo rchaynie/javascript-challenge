9de27cd3361f81ae5880cc64405707ac497a0f5e
Reid Haynie <reidhaynie@Reids-MacBook-Pro-2.local>
2020-11-24 15:16:46 -0600

    progress on js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -19,8 +19,27 @@ const handleClick = () => {
     const date = d3.select('#datetime').property('value')
     let filteredData = tableData;
 
-    if (date)
+    let date = d3.select('#datetime').property('value')
+    let city = d3.select("#city").property('value')
+    let state = d3.select('#state').property('value')
+    let country = d3.select('#country').property('value')
+    let shape = d3.select('#shape').property('value')
+
+    if (date) {
         filteredData = filteredData.filter(row => row.datetime === date)
+    }
+    if (city) {
+        filteredData = filteredData.filter(row => row.city === city)
+    }
+    if (state) {
+        filteredData = filteredData.filter(row => row.state === state)
+    }
+    if (country) {
+        filteredData = filteredData.filter(row => row.country === country)
+    }
+    if (shape) {
+        filteredData = filteredData.filter(row => row.shape === shape)
+    }
     
     tbody.html('')  // Clear existing data
 
