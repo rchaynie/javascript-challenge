2016f35c2593239c5ba73770c870eba6e9aef413
Reid Haynie <reidhaynie@Reids-MacBook-Pro-2.local>
2020-11-25 09:56:13 -0600

    part 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,7 +1,7 @@
 const tableData = data
-const tbody = d3.select('tbody')
+let tbody = d3.select('tbody')
 
-function buildTable(data) {
+//function buildTable(data) {
     tbody.html('')  // Clear existing data
 
     data.forEach(row => {
@@ -11,46 +11,49 @@ function buildTable(data) {
             cell.text(value)
         })
     })
-}
+//}
+let button = d3.select('#filter-btn');
+button.on('click', function(){
 
-const handleClick = () => {
-    d3.event.preventDefault()
+//const handleClick = () => {
+    //d3.event.preventDefault()
 
-    const date = d3.select('#datetime').property('value')
-    let filteredData = tableData;
+    let date = d3.select('#datetime').property('value');
+    //let filteredData = tableData;
 
-    let date = d3.select('#datetime').property('value')
-    let city = d3.select("#city").property('value')
-    let state = d3.select('#state').property('value')
-    let country = d3.select('#country').property('value')
-    let shape = d3.select('#shape').property('value')
+    // let date = d3.select('#datetime').property('value')
+    // let city = d3.select("#city").property('value')
+    // let state = d3.select('#state').property('value')
+    // let country = d3.select('#country').property('value')
+    // let shape = d3.select('#shape').property('value')
 
-    if (date) {
-        filteredData = filteredData.filter(row => row.datetime === date)
-    }
-    if (city) {
-        filteredData = filteredData.filter(row => row.city === city)
-    }
-    if (state) {
-        filteredData = filteredData.filter(row => row.state === state)
-    }
-    if (country) {
-        filteredData = filteredData.filter(row => row.country === country)
-    }
-    if (shape) {
-        filteredData = filteredData.filter(row => row.shape === shape)
-    }
+    // if (date) {
+        filteredData = tableData.filter(row => row.datetime === date)
+        console.log(filteredData)
+    // }
+    // if (city) {
+    //     filteredData = filteredData.filter(row => row.city === city)
+    // }
+    // if (state) {
+    //     filteredData = filteredData.filter(row => row.state === state)
+    // }
+    // if (country) {
+    //     filteredData = filteredData.filter(row => row.country === country)
+    // }
+    // if (shape) {
+    //     filteredData = filteredData.filter(row => row.shape === shape)
+    // }
     
     tbody.html('')  // Clear existing data
 
-    data.forEach(row => {
-        const currentRow = tbody.append('tr')  // append row
+    filteredData.forEach((row) => {
+        let currentRow = tbody.append('tr')  // append row
         Object.values(row).forEach(value => {
-            const cell = currentRow.append('td')
+            let cell = currentRow.append('td')
             cell.text(value)
-        })
-    })
-}
+        });
+    });
+});
+// }
 
-d3.selectAll('#filter-btn').on('click', handleClick)
 // buildTable(tableData)
